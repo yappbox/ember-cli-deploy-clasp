233f88cc446797747e8b004f5500283c977f6702
Luke Melia <luke@lukemelia.com>
2018-06-21 14:41:48 -0400

    Give up on detecting clasp install

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 /*eslint-env node*/
+/* global Promise */
 'use strict';
 
 const glob = require('glob');
-const detectInstalled = require('detect-installed');
 const util = require('util');
 const path = require('path');
 const execFile = util.promisify(require('child_process').execFile);
@@ -24,9 +24,9 @@ module.exports = {
        */
 
       defaultConfig: {
-        meaningOfLife: 42 // Example default config. Remove this.
+        distDir: 'dist'
       },
-      requiredConfig: ['isInNeedOfSleep'], // Example required config. Remove this;
+      requiredConfig: ['distDir'],
 
       /*
        * Implement any pipeline hooks here
@@ -34,33 +34,6 @@ module.exports = {
        * http://ember-cli-deploy.com/docs/v1.0.x/pipeline-hooks/
        */
 
-      configure(context) {
-        var claspInstalled = detectInstalled.sync('clasp');
-        if (!claspInstalled) {
-          throw new Error('@google/clasp must be installed and you must be logged in');
-        }
-      },
-
-      //setup(context) {
-      //  // Return an object with values you'd like merged in to the context to be accessed by other pipeline hooks and plugins
-      //  return {
-      //    someProp: 'someValue'
-      //  };
-      //},
-
-      //willDeploy(context) {
-      //  return RSVP.resolve(); // Return a promise if you'd like the pipeline to wait until the hook has performed some function
-      //},
-
-      //willBuild(context) {},
-      //build(context) {},
-      //didBuild(context) {},
-
-      //willPrepare(context) {},
-      //prepare(context) {},
-      //didPrepare(context) {},
-
-      //willUpload(context) {},
       upload(/*context*/) {
         var distDir = this.readConfig('distDir');
         var claspConfigFiles = glob.sync(`${distDir}/**/.clasp.json`);
@@ -68,25 +41,14 @@ module.exports = {
         claspConfigFiles.forEach((configFile) => {
           var claspProjectDir = path.dirname(configFile);
           process.chdir(claspProjectDir);
-          var claspPromise = execFile('clasp', 'push').then(({ stdout }/*, stderr*/) => {
+          this.log(`Running \`clasp push\` in ${claspProjectDir}`);
+          var claspPromise = execFile('clasp', ['push']).then(({ stdout }/*, stderr*/) => {
             this.log(stdout);
           });
           promises.push(claspPromise);
         });
         return Promise.all(promises);
-      },
-      //didUpload(context) {},
-
-      //willActivate(context) {},
-      //activate(context) {},
-      //didActivate(context) {},
-
-      //fetchInitialRevisions(context) {},
-      //fetchRevisions(context) {},
-
-      //didDeploy(context) {},
-
-      //teardown(context) {},
+      }
     });
 
     return new DeployPlugin();
